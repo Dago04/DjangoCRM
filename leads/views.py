3774eeffeafb54666e0ba2321bc04508b08309d6
Dago Salas <128066770+Dago04@users.noreply.github.com>
2024-01-24 21:51:51 -0600

    update class

--- leads/views.py
+++ leads/views.py
@@ -39,31 +39,17 @@ def lead_create(request):
     return render(request, "leads/lead_create.html", context)
 
 
-# def lead_create(request):
-#     form = LeadModelForm()
-#     # validar si el metodo es POST
-#     if request.method == "POST":
-#         print("Receiving a post request")
-#         form = LeadModelForm(request.POST)
-#         # validación del formulario
-#         if form.is_valid():
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
 
-#             # metodo para mostrar la data del form
-#             print(form.cleaned_data)
-#             first_name = form.cleaned_data["first_name"]
-#             last_name = form.cleaned_data["last_name"]
-#             age = form.cleaned_data["age"]
-#             # agarra el primer agent de la bd
-#             agent = Agent.objects.first()
-#             Lead.objects.create(
-#                 first_name=first_name,
-#                 last_name=last_name,
-#                 age=age,
-#                 agent=agent
-#             )
-
-#             return redirect("/leads")
-#     context = {
-#         "form": form
-#     }
-#     return render(request, "leads/lead_create.html", context)
+    context = {
+        "lead": lead,
+        "form": form
+    }
+    return render(request, "leads/lead_update.html", context)
