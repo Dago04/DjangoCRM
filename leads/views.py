533e3b136e1c7d34cc35897b137e9b4b02058582
Dago Salas <128066770+Dago04@users.noreply.github.com>
2024-01-24 21:33:22 -0600

    form

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
-
+from .models import Lead, Agent
+from .forms import LeadForm, LeadModelForm
 # Create your views here.
 
 
@@ -21,3 +21,49 @@ def lead_detail(request, pk):
         "lead": lead
     }
     return render(request, "leads/lead_detail.html", context)
+
+
+def lead_create(request):
+    form = LeadModelForm()
+    # validar si el metodo es POST
+    if request.method == "POST":
+        print("Receiving a post request")
+        form = LeadModelForm(request.POST)
+        # validación del formulario
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {
+        "form": form
+    }
+    return render(request, "leads/lead_create.html", context)
+
+
+# def lead_create(request):
+#     form = LeadModelForm()
+#     # validar si el metodo es POST
+#     if request.method == "POST":
+#         print("Receiving a post request")
+#         form = LeadModelForm(request.POST)
+#         # validación del formulario
+#         if form.is_valid():
+
+#             # metodo para mostrar la data del form
+#             print(form.cleaned_data)
+#             first_name = form.cleaned_data["first_name"]
+#             last_name = form.cleaned_data["last_name"]
+#             age = form.cleaned_data["age"]
+#             # agarra el primer agent de la bd
+#             agent = Agent.objects.first()
+#             Lead.objects.create(
+#                 first_name=first_name,
+#                 last_name=last_name,
+#                 age=age,
+#                 agent=agent
+#             )
+
+#             return redirect("/leads")
+#     context = {
+#         "form": form
+#     }
+#     return render(request, "leads/lead_create.html", context)
